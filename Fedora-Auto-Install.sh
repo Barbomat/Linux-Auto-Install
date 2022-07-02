b498c48470b2d21b66d4516371e71485bde07036
Matheus Barbosa <80155638+Barbomat@users.noreply.github.com>
2022-07-02 18:47:56 -0300

    v1
    First version of the script.

---
+++ Fedora-Auto-Install.sh
@@ -0,0 +1,177 @@
+# !/usr/bin/env bash
+
+
+# ================================================================== #
+# Autor       :   Matheus Barbosa <mdpb.matheus@gmail.com>.          #
+# Licença     :   MIT                                                #
+# Descrição   :   Script de automação para o sistema Fedora.         #
+# Referências :   Baseado na organização de código do Jefferson      #
+#                 'slackjeff' Rocha <root@slackjeff.com.br>.         #
+# ================================================================== #
+
+
+# ================================================================== #
+#                         ACELERANDO O DNF                           #
+# ================================================================== #
+
+sudo sed -i 's/max_parallel_downloads=3/max_parallel_downloads=20/g' /etc/dnf/dnf.conf
+printf "\n\n"
+
+
+# ================================================================== #
+#                     ADICIONANDO REPOSITÓRIOS                       #
+# ================================================================== #
+
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "\n\n"
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+printf "\n\n"
+
+flatpak remote-add appcenter --if-not-exists https://flatpak.elementary.io/repo.flatpakrepo
+printf "\n\n"
+
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+printf "\n\n"
+
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+printf "\n\n"
+
+sudo dnf install gstreamer1-plugins-{bad-*,good-*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+printf "\n\n"
+
+sudo dnf install lame* --exclude=lame-devel -y
+printf "\n\n"
+
+sudo dnf group upgrade --with-optional Multimedia -y
+printf "\n\n"
+
+# ================================================================== #
+#                             VARIÁVEIS                              #
+# ================================================================== #
+
+Programas_Remover_RPM=(
+    gnome-contacts
+    gnome-weather
+    gnome-clocks
+    gnome-maps
+    gnome-photos
+    gnome-connections
+    gnome-calculator
+    gnome-console
+    gnome-disk-utility
+    gnome-system-monitor
+    gnome-boxes
+    gparted
+    gnome-web
+    evince
+    gnome-characters
+    yelp
+    cheese
+    gnome-font-viewer
+    libreoffice
+    gnome-music
+    gnome-calendar
+    gnome-text-editor
+    gnome-logs
+    baobab
+    totem
+    eog
+    epiphany
+    evolution
+    gedit
+    mediawriter
+    gnome-software
+)
+
+Programas_Instalar_RPM=(
+    gnome-terminal
+    firefox
+    speedtest-cli
+    duf
+    nano
+    gtranslator
+    codium
+    rsms-inter-fonts
+    jetbrains-mono-nl-fonts
+    gnome-tweaks
+)
+
+
+# ================================================================== #
+#                ATUALIZANDO E LIMPANDO O SISTEMA                    #
+# ================================================================== #
+
+sudo dnf autoremove -y
+printf "\n\n"
+
+sudo dnf update -y
+printf "\n\n"
+
+flatpak update -y
+printf "\n\n"
+
+
+# ================================================================== #
+#                       REMOVENDO PROGRAMAS                          #
+# ================================================================== #
+
+for nome_do_programa in ${Programas_Remover_RPM[@]}; do
+
+    if ! dpkg -l | grep -q $nome_do_programa; then
+        echo "O programa $nome_do_programa não está instalado."
+        printf "\n\n"
+
+    else
+        sudo dnf autoremove $nome_do_programa -y
+        printf "\n\n"
+    fi
+
+done
+
+flatpak install spotify -y
+
+
+# ================================================================== #
+#                       INSTALANDO PROGRAMAS                         #
+# ================================================================== #
+
+for nome_do_programa in ${Programas_Instalar_RPM[@]}; do
+    
+    if ! dpkg -l | grep -q $nome_do_programa; then
+        sudo dnf install $nome_do_programa -y
+        printf "\n\n"
+
+    else
+        echo "O programa $nome_do_programa já está instalado."
+        printf "\n\n"
+    fi
+
+done
+
+
+# ================================================================== #
+#                          PÓS INSTALAÇÃO                            #
+# ================================================================== #
+
+sudo dnf update -y
+printf "\n\n"
+
+sudo dnf autoremove -y
+printf "\n\n"
+
+flatpak update -y
+printf "\n\n"
+
+flatpak remove --unused -y
+printf "\n\n"
+
+
+# ================================================================== #
+#                           O QUE FALTA                              #
+# ================================================================== #
+
+echo "A se Fazer:  Ir no Gnome Tweaks, mudar as fontes e reduzir a escala da tela para 0,90."
+echo "A se Fazer:  Ir no Google Drive e baixar o meu wallpaper pessoal."
+printf "\n\n"
+
---
+++ README.md
@@ -0,0 +1,8 @@
+## English
+
+This is a script for auto instalation and cleaning of programs in Fedora Linux, it's based on my personal configuration, so if you are gonna try this, please, change to your preferences before running.
+
+
+## Português Brasileiro
+
+Este é um script para instalação e limpeza automática de programas no Fedora Linux, e baseado em minha configuração pessoal, portanto, se você vai experimentar isto, por favor, mude para as suas preferências antes de executar.
